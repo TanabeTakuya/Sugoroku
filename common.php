9c521cc546d67b9bd5589db0d6356929aeecb057
manimanearly <manimani23230139@yahoo.co.jp>
2020-02-03 14:10:43 +0900

    agewasure

--- common.php
+++ common.php
@@ -1,8 +1,8 @@
 <?php
 function getDB1($sql, $param=[]){
-	$dsn = 'mysql:dbname=userdata;host=localhost';
-	$user = 'senpai';
-	$pw = 'indocurry';
+	$dsn = 'mysql:db1541=userdata;host=localhost';
+	$user = 'my1541';
+	$pw = 'recdautv';
 	
 	$dbh = new PDO($dsn, $user, $pw);
 	$sth = $dbh->prepare($sql);
