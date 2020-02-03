f37d43fcdf81aa7f95e6212ca5f3b5ec41278bc2
Tanabe Takuya <soranari-kuuya@ezweb.ne.jp>
2020-02-03 13:22:29 +0900

    cmi

--- userdata.php
+++ userdata.php
@@ -3,11 +3,11 @@
     $name = isset($_GET['name'])? $_GET['name']:null;
     $pw = isset($_GET['pw'])? $_GET['pw']:null;
     
-    $data = getDB1('select name, pw from userdata where name=?', [$name]);
+    $data = getDB1('select id, name, pw from userdata where name=?', [$name]);
 
     if( $data['pw'] == $pw){
-        echo "login";
-        header("location: play/room.html");
+        echo "aaaaa";
+        header("location: room.php?id=".$data['id']);
     }
     else{
 	    echo "名前かパスワードが間違っています。";
